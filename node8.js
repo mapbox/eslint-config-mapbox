c40ec4f1d97e03ee2d26173e89e98b98d256cfcc
David Clark <dclark@mapbox.com>
2018-03-28 08:23:09 -0700

    Lint configs linting themselves

--- node8.js
+++ node8.js
@@ -3,6 +3,6 @@
 module.exports = {
   extends: require.resolve('./lib/base-node'),
   rules: {
-    'node/no-unsupported-features': ['error', { version: 8 }],
+    'node/no-unsupported-features': ['error', { version: 8 }]
   }
 };
