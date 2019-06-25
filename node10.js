41bb8f6e9beb84384a1b835dec43bd88969e23ad
Matt Ficke <matt@mattficke.com>
2019-06-25 16:57:35 -0400

    fix typos

--- node10.js
+++ node10.js
@@ -3,8 +3,8 @@
 module.exports = {
   extends: require.resolve('./node'),
   rules: {
-    'node/no-unsupported-features/es-builtins': ['error', { version: 10 }],
-    'node/no-unsupported-features/es-syntax': ['error', { version: 10 }],
-    'node/no-unsupported-features/node-builtins': ['error', { version: 10 }]
+    'node/no-unsupported-features/es-builtins': ['error', { version: '10' }],
+    'node/no-unsupported-features/es-syntax': ['error', { version: '10' }],
+    'node/no-unsupported-features/node-builtins': ['error', { version: '10' }]
   }
 };
